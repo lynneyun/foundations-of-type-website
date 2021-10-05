dc22f605b24c8d68dbb11d03adb5944ceb4ea37f
Lynne <lynneyun@gmail.com>
2021-10-05 15:46:00 -0400

    saving progress after starting on section 2

---
+++ src/@rocketseat/gatsby-theme-docs/styles/theme.js
@@ -0,0 +1,23 @@
+export default {
+    colors: {
+        primary: '#44bd61',
+        background: '#FFFFFF',
+        shape: `#ebffe8`,
+        title: `#394d41`,
+        text: `#3b3b3b`,
+        components: {
+            blockquote: {
+                background: `#332616`,
+                text: `#E1E1E6`,
+            },
+        },
+    },
+    fonts: {
+        fontFace: {
+            family: 'Adelle Mono Flex',
+            file: '/fonts/AdelleMonoFlex-Regular',
+        },
+        body: 'Adelle Mono Flex',
+
+    },
+};
