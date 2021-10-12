a18bf9768583059304e5e0ac4404681886dad52d
Lynne <lynneyun@gmail.com>
2021-10-12 19:46:34 -0400

    fixed theme docs w Kevin

---
+++ src/@rocketseat/gatsby-theme-docs/components/Docs/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+
+import Layout from '@rocketseat/gatsby-theme-docs/src/components/Layout';
+import SEO from '@rocketseat/gatsby-theme-docs/src/components/SEO';
+import PostNav from '@rocketseat/gatsby-theme-docs/src/components/Docs/PostNav';
+
+export default function Docs({ mdx, pageContext }) {
+  const { prev, next, repositoryEditUrl, repositoryProvider } = pageContext;
+  const { title, description, image, disableTableOfContents } = mdx.frontmatter;
+  const { headings, body } = mdx;
+  const { slug } = mdx.fields;
+
+  return (
+    <>
+      <SEO title={title} description={description} slug={slug} image={image} />
+      <Layout
+        disableTableOfContents={disableTableOfContents}
+        title={title}
+        headings={headings}
+      >
+        <MDXRenderer>{body}</MDXRenderer>
+        
+        <PostNav prev={prev} next={next} />
+      </Layout>
+    </>
+  );
+}
+
+Docs.propTypes = {
+  mdx: PropTypes.shape({
+    body: PropTypes.string,
+    headings: PropTypes.array,
+    frontmatter: PropTypes.shape({
+      title: PropTypes.string,
+      description: PropTypes.string,
+      image: PropTypes.string,
+      disableTableOfContents: PropTypes.bool,
+    }),
+    fields: PropTypes.shape({
+      slug: PropTypes.string,
+    }),
+  }).isRequired,
+  pageContext: PropTypes.shape({
+    prev: PropTypes.shape({}),
+    next: PropTypes.shape({}),
+    repositoryEditUrl: PropTypes.string,
+    repositoryProvider: PropTypes.string,
+  }).isRequired,
+};
