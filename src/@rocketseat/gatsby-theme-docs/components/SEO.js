286f9be44038cecf536d021f962025267e153727
Lynne <lynneyun@gmail.com>
2021-10-20 12:20:44 -0400

    updating metadata

--- src/@rocketseat/gatsby-theme-docs/components/SEO.js
+++ src/@rocketseat/gatsby-theme-docs/components/SEO.js
@@ -111,9 +111,9 @@ SEO.propTypes = {
 };
 
 SEO.defaultProps = {
-  title: '',
-  description: '',
+  title: 'Type Design School',
+  description: 'An accessible online course for anyone to learn the basics of type design',
   slug: '',
-  image: '',
+  image: 'https://github.com/lynneyun/foundations-of-type-website/blob/master/static/thumbnail.png',
   children: '',
 };
